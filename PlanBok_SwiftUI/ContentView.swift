22487b1c9668f0c08a7d7f11e41a81a8676e70dd
Edem <edem.ekeng@live.com>
2020-06-09 00:25:39 -0400

    Initial Commit

---
+++ PlanBok_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PlanBok_SwiftUI
+//
+//  Created by Ed Em on 2020-06-09.
+//  Copyright © 2020 QuetzalConsults. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
